ca20d6e9c0c22e8c9c0d341070b8cffae0197a08
Mitch <mitch.mathias96@gmail.com>
2020-12-10 11:23:23 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const mysql = require("mysql");
+const express = require ("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        passsword: "",
+        database: "animals_db"
+    });
+}
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("CONNECTED!!")
+})
+ app.get ("/", function (req, res){
+     res.send("CONNECTED!")
+ })
+app.listen(PORT, function () {
+    console.log("you've connected at http://localhost:" + PORT)
+})
